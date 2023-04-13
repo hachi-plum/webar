fa70c851d63fde9f972d62f182e29090a64ac98d
hachi-plum <debayuru0901@gmail.com>
2023-04-13 13:30:09 +0900

    click

--- move.js
+++ move.js
@@ -1,9 +1,4 @@
-function moveModel() {
-    const model = document.querySelector('#cat-model');
-    // モデルの現在位置を取得
-    const currentPosition = model.position;
-    // 新しい位置を計算
-    const newPosition = {x: currentPosition.x + 0.1, y: currentPosition.y, z: currentPosition.z};
-    // 新しい位置にモデルを移動
-    model.position = newPosition;
-}
+const model = document.querySelector('#cat-model');
+model.addEventListener('click', () =>{
+    alert('Model clicked!');
+});
