60cb0d3d38cfd1342a1bba24a06d08e49e830ef7
hachi-plum <debayuru0901@gmail.com>
2023-04-13 13:35:02 +0900

    grrr

--- move.js
+++ move.js
@@ -1,4 +1,8 @@
 const model = document.querySelector('#cat-model');
 model.addEventListener('click', () =>{
-    alert('Model clicked!');
+    // 3Dモデルを移動する
+    const x = Math.random() * 4 - 2;
+    const y = Math.random() * 4 - 2;
+    const z = Math.random() * 4 - 2;
+    model.cameraControls.target = `${x} ${y} ${z}`;
 });
